4e9cd3380a91446b07999823b16c3a555a3f767f
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-03-19 15:01:52 +0100

    added dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   return (
-    <div className={styles.container} style={{backgroundColor: '#292929'}}>
+    <div className="bg-white h-screen justify-center items-center flex dark:bg-gray-800" >
       <Head>
         <title>tenseur</title>
       </Head>
-      <h1 style={{color: "white"}}>tenseur.</h1>
+      <h1 className="dark:text-white font-bold text-8xl text-gray-800">tenseur.</h1>
     </div>
   )
 }
