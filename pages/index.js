d658886df8ae9c039e9e5530c64da3974803f701
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-04-01 14:39:27 +0200

    added copyright mark.

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,15 @@ import 'tailwindcss/tailwind.css'
 
 export default function Home() {
   return (
-    <div className="bg-white h-screen justify-center items-center flex dark:bg-gray-800" >
+    <div className="bg-white h-screen justify-center items-center flex flex-col dark:bg-gray-800" >
       <Head>
         <title>tenseur</title>
         <meta name="description" content="tenseur website"/>
       </Head>
-      <h1 className="dark:text-white font-bold text-8xl text-gray-800">tenseur.</h1>
+      <main className="flex flex-grow justify-center items-center text-center">
+        <h1 className="dark:text-white font-bold text-8xl text-gray-800">tenseur.</h1>
+      </main>
+      <footer className="h-7"><small className="dark:text-gray-200 text-gray-600">&copy; tenseur 2021</small></footer>
     </div>
   )
 }
