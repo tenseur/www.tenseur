cac149ea3f7cb5dde05e820a3a29576002682f88
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-04-01 14:09:37 +0200

    added locale and meta page description.

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div className="bg-white h-screen justify-center items-center flex dark:bg-gray-800" >
       <Head>
         <title>tenseur</title>
+        <meta name="description" content="tenseur website"/>
       </Head>
       <h1 className="dark:text-white font-bold text-8xl text-gray-800">tenseur.</h1>
     </div>
